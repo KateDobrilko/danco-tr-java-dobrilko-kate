6ab540db61e750f5a14fdf457704dcdceb12019e
katedob1595 <katedob1595@gmail.com>
2015-04-01 18:02:32 +0000

    checkpoint1 job fixed.

---
+++ checkpoint1/Lab1/src/com/danco/training/dobrilko/main/Main.java
@@ -0,0 +1,11 @@
+package com.danco.training.dobrilko.main;
+
+import com.danco.training.dobrilko.facade.Facade;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Facade facade = new Facade();
+		facade.performAllWork();
+	}
+}
