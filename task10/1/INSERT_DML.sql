2d97729587c1bdcae69c6041e0fa442573e9e552
katedob1595 <katedob1595@gmail.com>
2015-04-13 00:24:37 +0000

    task10

---
+++ task10/1/INSERT_DML.sql
@@ -0,0 +1,56 @@
+INSERT INTO product VALUES ('Acer','K400','Laptop');
+INSERT INTO product VALUES ('HP','5478g','Laptop');
+INSERT INTO product VALUES ('HP','74565l','PC');
+INSERT INTO product VALUES ('Asus','L500','PC');
+INSERT INTO product VALUES ('Packard','K500','PC');
+INSERT INTO product VALUES ('Samsung','K895','PRINTER');
+INSERT INTO product VALUES ('LG','D545','PRINTER');
+INSERT INTO product VALUES ('Samsung','O500','PRINTER');
+
+INSERT INTO pc VALUES (1234, '74565l', 857,  256,  2, '1x', 454);
+INSERT INTO pc VALUES (6541, '74565l', 547,  512,  8,  '12x', 597);
+INSERT INTO pc VALUES (6542, '74565l', 454, 1024, 32, '12x',  909);
+
+INSERT INTO pc VALUES (654, 'L500', 6546, 1024,  32,  '12x', 546);
+INSERT INTO pc VALUES (6504, 'L500', 1666, 2048,  64, '12x', 865);
+INSERT INTO pc VALUES (985, 'L500', 454, 2048, 120, '24x', 1001);
+
+INSERT INTO pc VALUES (454, 'K500', 545, 1024, 1000,'24x', 545);
+INSERT INTO pc VALUES (8557, 'K500', 54, 1024, 666,  '24x',898);
+INSERT INTO pc VALUES (8808, 'K500', 87, 2048, 3000, '24x',1049);
+
+
+INSERT INTO laptop VALUES (6540, '5478g', 255, 128, 8, 199, 14);
+INSERT INTO laptop VALUES (5898, '5478g', 5433, 256, 16, 299, 14);
+INSERT INTO laptop VALUES (354, '5478g', 4585, 512, 30, 399, 17);
+
+INSERT INTO laptop VALUES (6542, 'K400', 222, 128, 8, 575, 14);
+INSERT INTO laptop VALUES (8954, 'K400', 666, 256, 33, 321, 14);
+INSERT INTO laptop VALUES (8545, 'K400', 888, 512, 64, 400, 14);
+
+
+INSERT INTO printer VALUES (6565, 'K895', 'n', 'Matrix', 100);
+INSERT INTO printer VALUES (3232, 'K895', 'y', 'Jet', 458);
+INSERT INTO printer VALUES (66, 'K895', 'n', 'Lader', 459);
+
+INSERT INTO printer VALUES (698998, 'D545', 'n', 'Matrix', 105);
+INSERT INTO printer VALUES (86894, 'D545', 'y', 'Lazer', 209);
+INSERT INTO printer VALUES (95545, 'D545', 'n', 'Lazer', 569);
+
+INSERT INTO printer VALUES (8996, 'O500', 'n', 'Matrix', 654);
+INSERT INTO printer VALUES (0665, 'O500', 'y', 'Lazer', 655);
+INSERT INTO printer VALUES (808, 'O500', 'y', 'Lazer', 987);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
